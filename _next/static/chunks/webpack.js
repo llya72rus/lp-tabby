5c4d08c5daa69fde8706d4539e821cd4059fdd9d
Ilya Makov <ilya.makov@tabby.ai>
2020-11-13 16:17:25 +0300

    shops number in different screen sizes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bfea0ce88a9e95bc3bd";
+/******/ 	var hotCurrentHash = "dc2543e52dc7132d884a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
