55cbc8111f2cd1df649d223180bc3320a09b12c3
Ilya Makov <ilya.makov@tabby.ai>
2020-11-13 15:47:36 +0300

    fixes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fb954f6bc4dd8dbdb111";
+/******/ 	var hotCurrentHash = "1bfea0ce88a9e95bc3bd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/stores/[category]":["static\u002Fchunks\u002Fpages\u002Fstores\u002F[category].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/ar":["static\u002Fchunks\u002Fpages\u002Far.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
