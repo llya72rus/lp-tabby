55a56da5e4e33074bce09518f64473535202f761
Ilya Makov <ilya.makov@tabby.ai>
2020-11-16 12:31:29 +0300

    rtl footer fixes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "18b96833248ae9a4e17b";
+/******/ 	var hotCurrentHash = "762fb2e916fbc343b8a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
