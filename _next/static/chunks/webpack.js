8a594ad13969110f8205ea4d01ee59b7c351591f
Ilya Makov <ilya.makov@tabby.ai>
2020-11-23 14:45:31 +0300

    black friday fixes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ef3126a14b79eedf526";
+/******/ 	var hotCurrentHash = "7d66a8a46eda30561c3e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
