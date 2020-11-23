9d5f63f51e921b2323e5d92291597904d9ea93ce
Ilya Makov <ilya.makov@tabby.ai>
2020-11-23 15:06:17 +0300

    bf fixes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d66a8a46eda30561c3e";
+/******/ 	var hotCurrentHash = "a638ff139218d65cb357";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
