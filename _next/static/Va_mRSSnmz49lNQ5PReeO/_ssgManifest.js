1aa3455300c56046aa5829c62f75ab76d9c334c3
Ilya Makov <ilya.makov@tabby.ai>
2020-11-23 13:27:09 +0300

    bf design fixes

---
+++ _next/static/Va_mRSSnmz49lNQ5PReeO/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Far","\u002F","\u002Fstores\u002F[category]","\u002Far\u002Fstores\u002F[category]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "11bb01cc71b99735b32f";
+/******/ 	var hotCurrentHash = "3ef3126a14b79eedf526";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
