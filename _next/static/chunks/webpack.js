480f43fa353ca515ff3857950b1adb95dc02d08a
Ilya Makov <ilya.makov@tabby.ai>
2020-12-09 22:09:00 +0300

    new header bg

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6047ad4a6edde03278ee";
+/******/ 	var hotCurrentHash = "561202d89e41f1216b63";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
